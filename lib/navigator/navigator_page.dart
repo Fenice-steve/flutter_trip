57610766ddc5ee69318216389cba2dbf38091556
fenice <fenicesteve@gmail.com>
2019-12-25 18:05:12 +0800

    新建导航页面

---
+++ lib/navigator/navigator_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class NavigatorPage extends StatefulWidget {
+  @override
+  _NavigatorPageState createState() => _NavigatorPageState();
+}
+
+class _NavigatorPageState extends State<NavigatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
