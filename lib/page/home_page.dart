bd248fdf2e7286c87699e1f84dfd5b7beaa9408d
fenice <fenicesteve@gmail.com>
2019-12-26 18:15:13 +0800

    完成appBar的滚动隐藏

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+// 滚动最大距离
+const APPBAR_SCROLL_OFFSET = 100;
 
 /// 首页
 class HomePage extends StatefulWidget {
@@ -7,8 +11,91 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List _UrlPage = [
+    "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3539459377,966183099&fm=26&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3539459377,966183099&fm=26&gp=0.jpg",
+    "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3539459377,966183099&fm=26&gp=0.jpg",
+  ];
+
+  double appBarAlpha = 0;
+
+  _onScroll(offset) {
+    // 滚动距离除以最大滚动距离
+    double alpha = offset / APPBAR_SCROLL_OFFSET;
+    // 处理滚动时的异常逻辑
+    if (alpha < 0) {
+      alpha = 0;
+    } else if (alpha > 1) {
+      alpha = 1;
+    }
+    setState(() {
+      // appBar的透明度
+      appBarAlpha = alpha;
+    });
+    print(appBarAlpha);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          MediaQuery.removePadding(
+            removeTop: true,
+            context: context,
+            child: NotificationListener(
+              onNotification: (scrollNotification) {
+                // 判断是否是监听更新的对象
+                if (scrollNotification is ScrollUpdateNotification &&
+                    // 从最外层Widget开始向下遍历查找
+                    scrollNotification.depth == 0) {
+                  // 滚动且是列表滚动的时候
+                  _onScroll(scrollNotification.metrics.pixels);
+                }
+              },
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    height: 160,
+                    child: Swiper(
+                      itemCount: _UrlPage.length,
+                      autoplay: true,
+                      // Swiper指示器
+                      pagination: SwiperPagination(),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Image.network(
+                          _UrlPage[index],
+                          fit: BoxFit.fill,
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 800,
+                    child: ListTile(
+                      title: Text("哈哈"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          // 透明Widget，opacity是必要参数
+          Opacity(
+            opacity: appBarAlpha,
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Text("首页"),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
