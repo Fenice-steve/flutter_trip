db29942b103dcfd33f9982f0b6f82097da5d7fc0
fenice <18816284864@163.com>
2020-01-06 23:52:16 +0800

    添加GridNav

--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -3,12 +3,32 @@ import 'package:flutter_trip/model/grid_nav_model.dart';
 
 class GridNav extends StatelessWidget {
   // Widget和它的所有子Widget都是不可变的，所以要用final
-  final GridNavModel gridNavModel;
+  final GridNavModel gridNav;
 
-  const GridNav({@required this.gridNavModel});
+  const GridNav({@required this.gridNav});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return PhysicalModel(color: Colors.transparent,
+    borderRadius: BorderRadius.circular(6),
+    clipBehavior: Clip.antiAlias,
+    child: Column(
+      children: <Widget>[
+
+      ],
+    ),);
+  }
+
+  List<Widget> _gridNavItems(BuildContext context){
+    List<Widget> items = [];
+    if(gridNav==null)return items;
+    if(gridNav.hotel != null){
+    }
+  }
+
+  Widget _gridNavItem(BuildContext context, GridNavItem gridNavItem, bool first){
+    List<Widget> items =[];
+    List<Widget> expandItems = [];
+
   }
 }
